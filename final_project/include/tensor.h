6e7abeb2dd52e21e776ab2afefc9a0cc6abe1852
satelite2517 <satelite2517@gmail.com>
2024-08-28 21:24:47 +0900

    Add GPU support to Tensor class - add gpu pointer

--- final_project/include/tensor.h
+++ final_project/include/tensor.h
@@ -11,9 +11,11 @@ struct Tensor {
   size_t ndim = 0;
   size_t shape[4];
   float *buf = nullptr;
+  float *gpu_buf = nullptr;
 
   Tensor(const vector<size_t> &shape_);
   Tensor(const vector<size_t> &shape_, float *buf_);
+  Tensor(const vector<size_t> &shape_, float *buf_, float *gpu_buf_);
   ~Tensor();
 
   size_t num_elem();
